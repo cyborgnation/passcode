3def1c4b3faec7786f9f616b312bf062e5b9c826
Joe Barber <iamjoebarber@gmail.com>
2020-02-28 12:24:54 -0600

    update passcode

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
  * Predeined arrays for various stuff
  */
 var terminalKeys = [
-  { val: 'D8', dots: 3 }, 
+  { val: 'D8', dots: 3 },
   { val: 'J5', dots: 2 },
   { val: 'K2', dots: 1 },
   { val: 'C1', dots: 2 },
@@ -41,7 +41,7 @@ var terminalTpl = "<header class='terminal-title'>" +
     "<span class='seq-pad__cnr cnr--bt-lt'></span>" +
     "<span class='seq-pad__cnr cnr--bt-rt'></span>" +
     "<div class='seq-pad__inr'>" +
-    
+
     "<% _.each(data, function(item,key){ %>" +
       "<% if(!item.val) { %>" +
         "<div class='pad-btn--blnk'></div>" +
@@ -60,7 +60,7 @@ var terminalTpl = "<header class='terminal-title'>" +
         "</button>" +
       "<% } %>" +
     "<% });  %>" +
-    
+
     "</div>" +
   "</div>" +
   "<div class='terminal-footer'>" +
@@ -74,34 +74,34 @@ var terminalTpl = "<header class='terminal-title'>" +
  * Set up backbone
  */
 var Panel = Backbone.Model.extend({
-  
+
   defaults: {
-  	passcode: ['C1', 'I3', '91', 'X0'],
-  	sequence: [],  
+  	passcode: ['C1', 'I3', '91', 'X0', '99'],
+  	sequence: [],
   },
 
   initialize: function(opts) {
     this.opts = opts;
     this.set('sequence', []);
-    
+
     this.listenTo(this, 'change', this.verify);
   },
-  
+
   addKey: function(key) {
     var temp = this.get('sequence');
     temp.push(key);
     this.set('sequence', temp);
     this.trigger('change');
   },
-  
+
   clearPass: function() {
     this.set('sequence', []);
   },
-  
+
   testSequence: function() {
     var i = 0,
         match = true;
-    
+
     for(var i=0; i< this.get('passcode').length; i++) {
       if(this.get('sequence')[i] !== this.get('passcode')[i]) {
         match = false;
@@ -110,19 +110,19 @@ var Panel = Backbone.Model.extend({
     }
     return match;
   },
-  
+
   isIncomplete: function() {
     return (this.isEmpty() || this.isFull()) ? false : true;
   },
-  
+
   isEmpty: function() {
     return (this.get('sequence').length == 0) ? true : false;
   },
-  
+
   isFull: function() {
     return (this.get('sequence').length == this.get('passcode').length) ? true : false;
   },
-  
+
   verify: function() {
     if (!this.isFull()) {
       return;
@@ -135,51 +135,51 @@ var Panel = Backbone.Model.extend({
 });
 
 var PanelView = Backbone.View.extend({
-  
+
   tagName: 'div',
   className: 'terminal',
-    
+
   model: null,
   template: null,
-  
+
   events: {
     'click .pad-btn': 'addKey',
     'click [pad-actn-clr]': 'resetForm'
   },
-  
+
   initialize: function(opts){
     this.sequence = []; // used to store the current sequence
-    
+
     this.opts = opts;
-    
+
     this.template = _.template(this.opts.template);
-    
+
     this.listenTo(this.model, 'change:sequence', this.toggleControls);
     this.listenTo(this.model, 'mismatch', this.setFailState);
     this.listenTo(this.model, 'match', this.setPassState);
     this.listenTo(this.model, 'destroy', this.trash);
   },
-  
+
   render: function(){
-    
+
     var _self = this;
-    
+
     this.$el.append(this.template({'data': this.opts.data }));
-    
+
     this.$btnPads    = this.$el.find('.pad-btn');
     this.$btnReset   = this.$el.find('[pad-actn-clr]');
     this.$msg        = this.$el.find('[pad-actn-msg]');
     this.$loadbar    = this.$el.find('.terminal-load-bar');
-    
+
     this.$el.on('animationend webkitAnimationEnd', function(){
       if (_self.$el.hasClass('state--warp')) {
         _self.model.destroy();
       }
     });
-    
+
     $('.login').append(this.$el);
   },
-  
+
   addKey: function(e){
     var $temp = $(e.target);
     if ($temp.hasClass('state--active')) {
@@ -188,12 +188,12 @@ var PanelView = Backbone.View.extend({
     $temp.addClass('state--active');
     this.model.addKey($temp.attr('key'));
   },
-  
+
   toggleControls: function(force) {
     var show = (force == true || force == false) ? force : this.model.isIncomplete();
     this.$btnReset[(show) ? 'addClass' : 'removeClass']('state--visible');
   },
-    
+
   setFailState: function() {
     this.showMessage({
       success: false,
@@ -202,7 +202,7 @@ var PanelView = Backbone.View.extend({
       formMsgClass: 'state--error'
     });
   },
-  
+
   setPassState: function() {
     this.showMessage({
       success: true,
@@ -211,19 +211,19 @@ var PanelView = Backbone.View.extend({
       formMsgClass: 'state--success'
     });
   },
-  
+
   showMessage: function(obj) {
-    
+
     var _self = this;
-    
+
     this.$btnPads.filter('.state--active')
                  .removeClass('state--active')
                  .addClass(obj.btnClass);
-    
+
     this.$el.addClass('state--frozen');
-    
+
     this.$msg.text(obj.formMsg).addClass('state--play ' + obj.formMsgClass);
-  
+
     if (!obj.success) {
       setTimeout(function(){
         _self.resetForm();
@@ -234,16 +234,16 @@ var PanelView = Backbone.View.extend({
       });
     }
   },
-  
+
   resetForm: function() {
     this.model.clearPass();
-  
+
   	this.$el.removeClass('state--frozen');
   	this.$btnPads.removeClass('state--active state--error state--success');
   	this.$msg.text('').removeClass('state--play');
   	this.toggleControls(false);
   },
-  
+
   loginForm: function() {
     var _self = this;
     this.$loadbar.addClass('state--loading')
@@ -251,7 +251,7 @@ var PanelView = Backbone.View.extend({
                     _self.$el.addClass('state--warp');
                  });
   },
-  
+
   trash: function() {
     toggleWelcome(true); // cheating
     this.undelegateEvents();
@@ -262,13 +262,13 @@ var PanelView = Backbone.View.extend({
 
 // terminal set up, so it can be called more than once
 var initTerminal = function() {
-  var login = null, 
+  var login = null,
       pad = null;
-  
+
   login = new Panel();
   pad = new PanelView({
-    model: login, 
-    template: terminalTpl, 
+    model: login,
+    template: terminalTpl,
     data: terminalKeys
   });
 
